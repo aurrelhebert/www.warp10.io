e7133ee9b8ede886b4b4de391782e7491534e141
Giwi <marin.xavier@gmail.com>
2017-11-02 18:01:19 +0100

    reboot in progress

--- _plugins/json.rb
+++ _plugins/json.rb
@@ -26,7 +26,7 @@ module Jekyll
       site.pages.each_with_index do |p, index|
         # Encode the HTML to JSON
         p.data['path'] = p.path
-        print "#{(100 * index / site.pages.size())}%                                     \r"
+     #   print "#{(100 * index / site.pages.size())}%                                     \r"
         hash = {:content => converter.convert(p.content), :title => p.data['title'], :desc => p.data['desc']}
         title = p.name.downcase.tr(' ', '-').delete('’!')
         title.slice! '.md'
@@ -55,8 +55,10 @@ module Jekyll
         unless menu.has_key?(catPath)
           menu[catPath] = Array.new
         end
-        menu[catPath].push({:title => p.data['title'], :path => path})
 
+        menu[catPath].push({:title => p.data['title'], :path => path})
+puts p.data['title']
+        puts path
         # Create the JSON file and inject the data
         f = File.new("_site/raw/#{path}/raw.json", 'w+')
         f.puts JSON.generate(hash)
